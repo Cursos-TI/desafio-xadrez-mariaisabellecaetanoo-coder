aa4a85eb8a468dbc76faec54d9abc83f3e934ff2
bebel <mariaisabellecaetanoo@gmail.com>
2026-05-20 21:32:35 +0000

    FIZ O Desafio: nível aventureiro Movimentando o Cavalo

--- xadrez.c
+++ xadrez.c
@@ -2,31 +2,35 @@
 
 int main() {
 
-    // Variáveis com a quantidade de casas
+    // Quantidade de casas que cada peça irá mover
     int casasTorre = 5;
     int casasBispo = 5;
     int casasRainha = 8;
 
-    int i;
+    // Movimento do cavalo
+    int movimentosBaixo = 2;
+    int movimentosEsquerda = 1;
 
-    // ----------------------------
-    // Movimento da Torre (FOR)
+    int i;
+    int j;
+
+    // ======================================
+    // Movimento da Torre usando FOR
     // Torre: 5 casas para a direita
-    // ----------------------------
+    // ======================================
     printf("Movimento da Torre:\n");
 
     for(i = 1; i <= casasTorre; i++) {
         printf("Direita\n");
     }
 
-    // Linha em branco para separar
     printf("\n");
 
-    // ----------------------------
-    // Movimento do Bispo (WHILE)
+    // ======================================
+    // Movimento do Bispo usando WHILE
     // Bispo: 5 casas na diagonal
     // para cima e à direita
-    // ----------------------------
+    // ======================================
     printf("Movimento do Bispo:\n");
 
     i = 1;
@@ -36,13 +40,12 @@ int main() {
         i++;
     }
 
-    // Linha em branco para separar
     printf("\n");
 
-    // ----------------------------
-    // Movimento da Rainha (DO-WHILE)
+    // ======================================
+    // Movimento da Rainha usando DO-WHILE
     // Rainha: 8 casas para a esquerda
-    // ----------------------------
+    // ======================================
     printf("Movimento da Rainha:\n");
 
     i = 1;
@@ -51,6 +54,31 @@ int main() {
         printf("Esquerda\n");
         i++;
     } while(i <= casasRainha);
+
+    printf("\n");
+
+    // ======================================
+    // Movimento do Cavalo usando
+    // loops aninhados (FOR + WHILE)
+    //
+    // Cavalo:
+    // 2 casas para baixo
+    // 1 casa para a esquerda
+    // ======================================
+    printf("Movimento do Cavalo:\n");
+
+    // Loop FOR para mover para baixo
+    for(i = 1; i <= movimentosBaixo; i++) {
+        printf("Baixo\n");
+    }
+
+    // Loop WHILE para mover para a esquerda
+    j = 1;
+
+    while(j <= movimentosEsquerda) {
+        printf("Esquerda\n");
+        j++;
+    }
 
     return 0;
 }
